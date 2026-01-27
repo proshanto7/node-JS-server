5568e9a83920fa2737e50c3ccfd3aa134bd57f6f
proshanto7 <proshantosarker112@gmail.com>
2026-01-27 17:52:28 +0600

    file systeam data read

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const fs = require('fs');
+const fs = require("fs");
 
-fs.writeFile("userData.txt", "hello users", (error) => { console.log(error) })
+// fs.writeFile("userData.txt", "hello users", (error) => { console.log(error) })
 
-
+fs.readFile("userData.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
