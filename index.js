9b39df54f2f1b3273c5ca56e80e89bed7bc39ba0
proshanto7 <proshantosarker112@gmail.com>
2026-01-28 21:48:35 +0600

    import path module

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const os = require('os');
+const path = require('path');
 
+// console.log(os.freemem() / (1024 * 1024 * 1024));
 
-console.log(os.freemem()/ (1024 * 1024 *1024));
+const filename = path.basename('/users/docs/file.txt');
+console.log(filename);
